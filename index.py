bf296b4e643e20cf4619df9452fc34f8f789bf62
Peter Nguyen <peter@datacrunch.ca>
2022-08-26 18:06:59 -0400

    Update index.py

--- index.py
+++ index.py
@@ -6,6 +6,14 @@ from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExport
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.jinja2 import Jinja2Instrumentor
 
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
+from opentelemetry.sdk._logs import LogEmitterProvider
+from opentelemetry.sdk._logs import set_log_emitter_provider
+from opentelemetry.sdk._logs import LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogProcessor
+
+import logging
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -15,9 +23,40 @@ serviceId = str(uuid.uuid1())
 trace.set_tracer_provider(TracerProvider(resource=Resource.create({"service.name": "demo-flask.otel", "service.instance.id": serviceId, "tag.team": "datacrunch-vercel"})))
 trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(OTLPSpanExporter()))
 
+log_emitter_provider = LogEmitterProvider(resource=Resource.create({"service.name": "demo-flask.otel", "service.instance.id": serviceId, "tag.team": "datacrunch-vercel"}))
+set_log_emitter_provider(log_emitter_provider)
+
+exporter = OTLPLogExporter(insecure=True)
+log_emitter_provider.add_log_processor(BatchLogProcessor(exporter))
+log_emitter = log_emitter_provider.get_log_emitter(__name__, "0.1")
+handler = LoggingHandler(level=logging.NOTSET, log_emitter=log_emitter)
+
+logging.getLogger().addHandler(handler)
+
 FlaskInstrumentor().instrument_app(app)
 Jinja2Instrumentor().instrument()
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@flaskapp.route("/ping", strict_slashes=False)
+def ping():
+    return jsonify(ping="pong")
+
+# API to convert Fahrenheit to Celcius
+@flaskapp.route("/convertC/<tempF>")
+def convertC(tempF):
+    tempC = (5/9*(float(tempF))-32)
+    logging.info(f"[INFO] Converted {tempF}°F to {tempC:.2f}°C.")
+    return f"{tempF}°F is {tempC:.2f}°C."
+
+# API to convert Celcius to Fahrenheit New Comment
+@flaskapp.route("/convertF/<tempC>")
+def convertF(tempC):
+    try:
+        tempF = 9/5*(float(tempC))+32
+        logging.info(f"[INFO] Converted {tempC}°F to {tempF:.2f}°C.")
+        return f"{tempC}°C is {tempF:.2f}°F."
+    except:
+        logging.warning("[WARN] Invalid temperature!")
