57eafec61b56af0381f5c8b60d1b2db5784f25e9
Peter Nguyen <peter@datacrunch.ca>
2022-08-26 17:16:05 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,23 @@
 import newrelic.agent
 newrelic.agent.initialize()
 
-from flask import Flask
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
+from flask import Flask
 app = Flask(__name__)
+
+import uuid
+serviceId = str(uuid.uuid1())
+
+trace.set_tracer_provider(TracerProvider(resource=Resource.create({"service.name": "demo-flask.otel", "service.instance.id": serviceId, "tag.team": "datacrunch-prod"})))
+trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(OTLPSpanExporter()))
+
+FlaskInstrumentor().instrument_app(app)
 
 @app.route('/')
 def hello_world():
